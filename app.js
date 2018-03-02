c746db8ac42880a92d5dfdd2966367190be88812
Luke Boga Mitchell <lbogamitchell@gmail.com>
2018-03-02 12:14:20 +0000

    Basic endpoint working

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => res.send("<h1>HS-BT</h1>"));
@@ -11,7 +13,8 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var checkout = require('./routes/checkout');
 app.use('/checkout', checkout);
-
-module.exports = app;
