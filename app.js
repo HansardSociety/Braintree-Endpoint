2c7e0d788caa1a3a8f92eeef17520b96d5984926
Luke Boga Mitchell <lbogamitchell@gmail.com>
2018-03-02 11:46:47 +0000

    Basic BT setup (not tested)

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const app = express();
 app.get("/", (req, res) => res.send("<h1>HS-BT</h1>"));
 
 app.listen(3000, () => console.log("Listening on 3000"));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var checkout = require('./routes/checkout');
+app.use('/checkout', checkout);
+
+module.exports = app;
