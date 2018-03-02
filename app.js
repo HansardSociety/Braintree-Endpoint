62991f186a29d1d2c1390656b19d9e65de264794
Luke Boga Mitchell <lbogamitchell@gmail.com>
2018-03-02 12:23:17 +0000

    Use express http skelaton

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("<h1>HS-BT</h1>"));
 
-app.listen(3000, () => console.log("Listening on 3000"));
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,3 +16,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var checkout = require('./routes/checkout');
 app.use('/checkout', checkout);
+
+module.exports = app
