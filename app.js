ef8e14516dd9ed389c27b18d86a5b525cf52c4ae
LukeBM <luke.bogamitchell@gmail.com>
2018-03-04 15:04:27 +0000

    Change Express to https and install Helmet

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("<h1>HS-BT</h1>"));
 
+app.use(require("helmet")());
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,7 +16,7 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var checkout = require('./routes/checkout');
-app.use('/checkout', checkout);
+var checkout = require("./routes/checkout");
+app.use("/checkout", checkout);
 
 module.exports = app
